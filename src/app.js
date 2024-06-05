959399c54d0b39c13e0db2bf8666d92b4c207147
Dunconius <duncanbean84@gmail.com>
2024-06-06 06:10:19 +1000

    feat: router with posts

--- src/app.js
+++ src/app.js
@@ -2,11 +2,17 @@
 
 const express = require("express");
 const serverInstance = express();
+const PokemonRouter = require("./routers/pokemonRoutes.js");
 
 // raw json in body allowed
 serverInstance.use(express.json());
 // form data in body allowed
 serverInstance.use(express.urlencoded({extended: true}));
+
+// every route that begins with /pokemon gets passed to PokemonRouter
+serverInstance.use("/pokemon", PokemonRouter);
+
+
 
 serverInstance.get("/", (request, response) => {
     console.log("hi from the server homepage updated!");
