08b70a94b50688e358899bf0fbafead28c0876e8
Dunconius <duncanbean84@gmail.com>
2024-06-06 20:25:50 +1000

    feat: example middlewares

--- src/app.js
+++ src/app.js
@@ -1,36 +1,61 @@
-// declare and configure server
+// Declare and configure the server
 
 const express = require("express");
 const serverInstance = express();
-const PokemonRouter = require("./routers/pokemonRoutes.js");
 
-// raw json in body allowed
+
+// Raw JSON in body allowed
 serverInstance.use(express.json());
-// form data in body allowed
+// Form data in body allowed
 serverInstance.use(express.urlencoded({extended: true}));
 
-// every route that begins with /pokemon gets passed to PokemonRouter
+
+
+
+// Every route that begins with /pokemon gets passed to PokemonRouter
+const PokemonRouter = require("./routers/pokemonRoutes.js");
 serverInstance.use("/pokemon", PokemonRouter);
+serverInstance.use("/digimon", PokemonRouter);
+serverInstance.use("/food", PokemonRouter);
+serverInstance.use("/countries", PokemonRouter);
+
+
+
+
 
 
 
 serverInstance.get("/", (request, response) => {
-    console.log("hi from the server homepage updated!");
+	console.log("Someone visited the homepage of the server");
 
-    response.json({
-        message: "hello world"
-    });
+	response.json({
+		message:"Hello world! Alex was here!"
+	});
 });
 
 serverInstance.post("/", (request, response) => {
-    
-    console.log(request.body);
 
-    response.json({
-        message:"Received data:",
-        requestData: request.body
-    })
-})
+	console.log(request.body);
+
+	response.json({
+		message:"Received data:",
+		requestData: request.body
+	})
+});
+
+serverInstance.put("/", (request, response) => {
+	response.json({message:"Put request received"})
+});
+
+serverInstance.patch("/", (request, response) => {
+	response.json({message:"Patch request received"})
+});
+
+serverInstance.delete("/", (request, response) => {
+	response.json({message:"Delete request received"})
+});
 
 
-module.exports = serverInstance
+
+// Make the instance available for other files to use
+module.exports = serverInstance;
