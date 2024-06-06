b6db638e604fb9c336ed59008bbdbf473945c45c
Dunconius <duncanbean84@gmail.com>
2024-06-06 19:41:58 +1000

    feat: route and query params

--- src/routers/pokemonRoutes.js
+++ src/routers/pokemonRoutes.js
@@ -5,7 +5,24 @@ router.get("/", (request, response) => {
     response.json({message:"Router route activated!"});
 });
 
+router.get("/getbyid/:pokemonNumber", (request, response) => {
+    
+    let retrievedNumberFromUrl = request.params.pokemonNumber;
+    
+    response.json({
+        number: retrievedNumberFromUrl
+    })
+});
+
 router.get("/random", (request, response) => {
+    let queryParams = request.query;
+    response.json({
+        message:"Random Route Routed!!",
+    queryParams: queryParams
+    });
+});
+
+router.post("/random", (request, response) => {
     response.json({message:"Random Route Routed!!"});
 });
 
